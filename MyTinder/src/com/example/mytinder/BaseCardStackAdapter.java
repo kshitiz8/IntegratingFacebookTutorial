63b2f795e500be3ce3185febbca79beeb56de9c0
dwivedi <dwivedi@yahoo-inc.com>
2014-08-31 17:32:44 -0700

    First commit for slider app

---
+++ MyTinder/src/com/example/mytinder/BaseCardStackAdapter.java
@@ -0,0 +1,12 @@
+package com.example.mytinder;
+
+import android.widget.BaseAdapter;
+
+import com.example.mytinder.CardModel;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public abstract class BaseCardStackAdapter extends BaseAdapter {
+
+}
